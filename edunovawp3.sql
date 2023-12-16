f68b121ae6f8db083681aa1e7acf82ca36a28cd0
Mem28 <jisse.merlin@gmail.com>
2023-12-16 11:52:20 +0100

    Update edunovawp3.sql

--- edunovawp3.sql
+++ edunovawp3.sql
@@ -11,7 +11,7 @@ use edunovawp3;
 
 create table smjerovi(
 sifra int not null primary key identity(1,1),
-naziv varchar(50) not null,
+naziv varchar(100) not null,
 trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumjeva null
 cijena decimal(18,2), -- iako ništa ne piše je null
 vaucer bit
