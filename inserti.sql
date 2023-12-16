4931459cdd553d738c7cc358f17941f5600a40cb
Mem28 <jisse.merlin@gmail.com>
2023-12-16 12:06:19 +0100

    Mijenjanje podataka

--- inserti.sql
+++ inserti.sql
@@ -2,19 +2,6 @@ use edunovawp3;
 
 select * from smjerovi;
 
-insert into smjerovi (naziv,trajanje,cijena,vaucer)
-values ('Web programiranje', 225, 1859.45, 1);
-
-insert into smjerovi values
-('Web dizajn', null, null, null);
-
-insert into smjerovi(naziv) values ('čadkč ČDJK');
-
-
-insert into smjerovi(naziv,trajanje,cijena,vaucer)
-values ('knjigovodstvo', 185, 1324.34,1);
-
-delete from smjerovi;
 
 insert into smjerovi (naziv,trajanje,cijena,vaucer)
 values ('web programiranje', 225, 1911.10, 1);
@@ -22,6 +9,36 @@ values ('web programiranje', 225, 1911.10, 1);
 insert into smjerovi(naziv) values ('web dizajn');
 insert into smjerovi(naziv) values ('knjigovodstvo');
 insert into smjerovi(naziv) values ('engleski jezik');
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('java developer',130, 922.42,0);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('php developer',130, 922.42,0);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('android developer',130, null ,0);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('wordpress developer',20, null,0);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('javascript developer',30, null ,0);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('woocommerce trening',24, null,null);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('3D print dizajner',130, null,null);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('serviser osobnih računala',155, null,null);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('grafički dizajner',150, null,null);
+
+insert into smjerovi(naziv,trajanje,cijena,vaucer)
+values ('računalni operater',155, 371.62,null);
 
 select * from predavaci;
 
