b5d1a98e8cced004c84b6dffb5da9772e76d7df9
Mem28 <jisse.merlin@gmail.com>
2024-01-27 13:09:50 +0100

    Gdje počinje i gdje završava ruta

--- CSHARP/UcenjeWP3/WebAPI/Controllers/E01Ulazizlaz.cs
+++ CSHARP/UcenjeWP3/WebAPI/Controllers/E01Ulazizlaz.cs
@@ -6,12 +6,30 @@ namespace WebAPI.Controllers
     [Route("E01")]
     public class E01Ulazizlaz:ControllerBase
     {
+
+
+        //ovdjee počinje ruta
         [HttpGet]
         [Route("Hello")]
         public string HelloWorld (string Ime)
         {
             return "Dobro večer " + Ime;
         }
+        //ovdje završava ruta
 
+
+
+
+
+
+
+        //ovdjee počinje ruta
+        [HttpGet]
+        [Route("zad1")]
+        public int Zad1(int prvi, int drugi)
+        {
+            return prvi + drugi;
+        }
+        //ovdje završava ruta
     }
 }
