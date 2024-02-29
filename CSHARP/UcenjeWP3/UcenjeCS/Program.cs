7b91012f0bd55674a187d09f686a41c08e305e8e
Mem28 <jisse.merlin@gmail.com>
2024-02-29 21:02:59 +0100

    Polimorfizam

--- CSHARP/UcenjeWP3/UcenjeCS/Program.cs
+++ CSHARP/UcenjeWP3/UcenjeCS/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using UcenjeCS;
-new UcenjeCS.E14VjezbanjeRadaSObjektima.Program();
+new UcenjeCS.E15Polimorfizam.Program();
+//new UcenjeCS.E14VjezbanjeRadaSObjektima.Program();
 //Kada vidimo kljucnu riječ 'new' poziva se konstruktor
 //new UcenjeCS.E13Nasljedivanje.Program();//pozvao sam konstruktor od Programa u E13
 //UcenjeCS.E12KlasaObjekt.Program.Izvedi();
